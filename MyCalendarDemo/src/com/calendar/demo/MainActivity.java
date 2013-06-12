2601d1ff3d7f5cd1c71aaa360c9f94ea464a26bc
lanyunzhang <zhanglanyun.zly@gmail.com>
2013-06-12 18:11:57 +0800

    计划备忘分开界面30%

--- MyCalendarDemo/src/com/calendar/demo/MainActivity.java
+++ MyCalendarDemo/src/com/calendar/demo/MainActivity.java
@@ -14,12 +14,17 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
@@ -28,6 +33,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -83,7 +90,6 @@ import com.calendar.util.util;
  *   12. 实现键盘向上移动的时，可以动态变化输入框的行数
  *   13. 计划和备忘的切换开关实现 
  *   14. 添加闹钟逻辑，可以选择多个闹钟，同时列出多个闹钟的时间
- *   
  *   15. 在添加事件界面，不能去点击日历的左右切换和日历文本选择逻辑
  */
 public class MainActivity extends Activity{
@@ -169,6 +175,15 @@ public class MainActivity extends Activity{
 	public static int Calendar_WeekFontColor = 0;
 
 	String UserName = "";
+	
+	//----选项卡
+	private ViewPager mPager;//页卡内容
+    private List<View> listViews; // Tab页面列表
+    private ImageView cursor;// 动画图片
+    private TextView t1, t2, t3;// 页卡头标
+    private int offset = 0;// 动画图片偏移量
+    private int currIndex = 0;// 当前页卡编号
+    private int bmpW;// 动画图片宽度
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -304,6 +319,118 @@ public class MainActivity extends Activity{
 		});
 		selectday =iDay + calToday.get(Calendar.DAY_OF_MONTH);
 		System.out.println("onCreate---"+selectday);
+         
+        InitImageView();
+ 		InitTextView();
+ 		InitViewPager();
+	}
+	
+	/**
+	 * 初始化头标
+	 */
+	private void InitTextView() {
+		t1 = (TextView) findViewById(R.id.text1);
+		t2 = (TextView) findViewById(R.id.text2);
+		t3 = (TextView) findViewById(R.id.text3);
+
+		t1.setOnClickListener(new MyOnClickListener(0));
+		t2.setOnClickListener(new MyOnClickListener(1));
+		t3.setOnClickListener(new MyOnClickListener(2));
+	}
+
+	/**
+	 * 初始化ViewPager
+	 */
+	private void InitViewPager() {
+		mPager = (ViewPager) findViewById(R.id.vPager);
+		listViews = new ArrayList<View>();
+		LayoutInflater mInflater = getLayoutInflater();
+		listViews.add(mInflater.inflate(R.layout.lay1, null));
+		listViews.add(mInflater.inflate(R.layout.lay2, null));
+		listViews.add(mInflater.inflate(R.layout.lay3, null));
+		mPager.setAdapter(new MyPagerAdapter(listViews));
+		mPager.setCurrentItem(0);
+		mPager.setOnPageChangeListener(new MyOnPageChangeListener());
+	}
+
+	/**
+	 * 初始化动画
+	 */
+	private void InitImageView() {
+		cursor = (ImageView) findViewById(R.id.cursor);
+		bmpW = BitmapFactory.decodeResource(getResources(), R.drawable.a)
+				.getWidth();// 获取图片宽度
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		int screenW = dm.widthPixels;// 获取分辨率宽度
+		offset = (screenW / 3 - bmpW) / 2;// 计算偏移量
+		Matrix matrix = new Matrix();
+		matrix.postTranslate(offset, 0);
+		cursor.setImageMatrix(matrix);// 设置动画初始位置
+	}
+	/**
+     * 头标点击监听
+     */
+    public class MyOnClickListener implements View.OnClickListener {
+        private int index = 0;
+
+        public MyOnClickListener(int i) {
+            index = i;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mPager.setCurrentItem(index);
+        }
+    };
+    
+    /**
+	 * 页卡切换监听
+	 */
+	public class MyOnPageChangeListener implements OnPageChangeListener {
+
+		int one = offset * 2 + bmpW;// 页卡1 -> 页卡2 偏移量
+		int two = one * 2;// 页卡1 -> 页卡3 偏移量
+
+		@Override
+		public void onPageSelected(int arg0) {
+			Animation animation = null;
+			switch (arg0) {
+			case 0:
+				if (currIndex == 1) {
+					animation = new TranslateAnimation(one, 0, 0, 0);
+				} else if (currIndex == 2) {
+					animation = new TranslateAnimation(two, 0, 0, 0);
+				}
+				break;
+			case 1:
+				if (currIndex == 0) {
+					animation = new TranslateAnimation(offset, one, 0, 0);
+				} else if (currIndex == 2) {
+					animation = new TranslateAnimation(two, one, 0, 0);
+				}
+				break;
+			case 2:
+				if (currIndex == 0) {
+					animation = new TranslateAnimation(offset, two, 0, 0);
+				} else if (currIndex == 1) {
+					animation = new TranslateAnimation(one, two, 0, 0);
+				}
+				break;
+			}
+			currIndex = arg0;
+			animation.setFillAfter(true);// True:图片停在动画结束位置
+			animation.setDuration(300);
+			cursor.startAnimation(animation);
+		}
+
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+		}
 	}
 	
 	class CustomerDatePickerDialog extends DatePickerDialog {
